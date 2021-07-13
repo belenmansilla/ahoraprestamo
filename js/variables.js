4766d39b0d405ea79ffeab2b2234a83f50022a16
belenmansilla <mansilla.cb@gmail.com>
2021-07-13 19:29:03 -0300

    Se agregan modificaciones

--- js/variables.js
+++ js/variables.js
@@ -5,7 +5,7 @@ let mail = prompt("Ingresá tu correo electrónico");
 if (monto <= 1000) {
     alert ("Hola " + nombre + " tu solicitud de prestamo por un monto de " + monto + " fue aprobado. Te llegará un mail al correo: " + mail + " en las próximas 24 horas hábiles para que puedas retirar tu dinero.");
 }
-else if (monto > 1000) {
+else {
     alert ("Hola " + nombre + " tu solicitud de prestamo por un monto de " + monto + " está en revisión. Te llegará un mail al correo: " + mail + " en las próximas 24 horas hábiles con el estado de tu solicitud.");
 }
 
