c321adce65924a1dd477823ac80520903d9f8954
belenmansilla <mansilla.cb@gmail.com>
2021-07-13 21:20:38 -0300

    Se agrega while

--- js/variables.js
+++ js/variables.js
@@ -1,12 +1,19 @@
 let nombre = prompt("Nombre Completo");
-let monto = prompt("Monto deseado en pesos");
 let mail = prompt("Ingresá tu correo electrónico");
+let monto = prompt("Monto deseado en pesos");
+let cuotas = prompt("Ingrese la cantidad de cuotas que te gustaría");
+let seguir = prompt("¿Te gustaría cotizar otro prestamo? Y/N");
+
+while (seguir == "Y") {
+    monto = prompt("Monto deseado en pesos");
+    cuotas = prompt("Ingrese la cantidad de cuotas que te gustaría");
+    seguir = prompt("¿Te gustaría cotizar otro prestamo? Y/N");
+}
 
 if (monto <= 1000) {
-    alert ("Hola " + nombre + " tu solicitud de prestamo por un monto de " + monto + " fue aprobado. Te llegará un mail al correo: " + mail + " en las próximas 24 horas hábiles para que puedas retirar tu dinero.");
+alert ("Hola " + nombre + " tu solicitud de prestamo por un monto de " + monto + " fue aprobado. \n \nTe llegará un mail al correo: " + mail + " en las próximas 24 horas hábiles para que puedas retirar tu dinero.");
 }
 else {
-    alert ("Hola " + nombre + " tu solicitud de prestamo por un monto de " + monto + " está en revisión. Te llegará un mail al correo: " + mail + " en las próximas 24 horas hábiles con el estado de tu solicitud.");
+    alert ("Hola " + nombre + " tu solicitud de prestamo por un monto de " + monto + " está en revisión.\n \nTe llegará un mail al correo: " + mail + " en las próximas 24 horas hábiles con el estado de tu solicitud.");
 }
 
-console.log("El usuario solicitó un prestamo de " + monto)
